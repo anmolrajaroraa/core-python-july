76578de74d1b5aedf2de82755db8dd868ed3c690
Anmol Arora <anmolarora1711@gmail.com>
2020-07-20 13:49:35 +0530

    Update turtle-for-loop.py

--- turtle-for-loop.py
+++ turtle-for-loop.py
@@ -22,14 +22,46 @@ pen.speed(10)
 
 '''
 for i in range(30):
-    print(15*i)
+    #print(15*i)
     pen.forward(15*i)
     pen.left(120)
 '''
 
-for i in range(10):
+'''
+pen.fillcolor('darkblue')
+pen.begin_fill()
+for i in range(1):
    for j in range(4):
-        pen.forward(30 * i)
+        pen.forward(300)
         pen.left(90)
 
+pen.end_fill()'''
+'''
 for i in range(50): pen.circle(5 * i)
+'''
+
+#for i in range(10):
+while True:   # infinite loop
+    user_input = input("Enter shape name:")
+    if user_input == "circle":
+        pen.circle(100)
+    elif user_input == "square":
+        for i in range(4):
+            pen.forward(200)
+            pen.left(90)
+    elif user_input == "triangle":
+        for i in range(3):
+            pen.forward(200)
+            pen.left(120)
+    elif user_input == "hexagon":
+        for i in range(6):
+            pen.forward(150)
+            pen.left(60)
+    elif user_input == "pentagon":
+        for i in range(5):
+            pen.forward(200)
+            pen.left(72)
+    else:
+        print("Shape doesn't exist")
+            
+   
