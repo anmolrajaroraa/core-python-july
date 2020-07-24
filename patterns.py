f80173f763de7579e50dcefe2438aadd39da5f79
Anmol Arora <anmolarora1711@gmail.com>
2020-07-24 13:37:29 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -29,3 +29,40 @@ for i in range(5):
         print('*', end='')
 
     print()
+
+
+'''
+    *
+   ***
+  *****
+ *******
+*********
+
+A
+B C 
+D E F
+G H I J
+K L M N O
+
+a
+a b
+a b c
+a b c d
+a b c d e
+'''
+
+asciiCode = 65
+for i in range(5):
+    for j in range(i + 1):
+        print(chr(asciiCode), end=" ")
+        asciiCode += 1
+    print()
+
+print("********************")
+
+for i in range(5):
+    for j in range(4 - i):
+        print(" ", end='')
+    for k in range(2 * i + 1):
+        print('*', end='')
+    print()
