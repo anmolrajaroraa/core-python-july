6e1f0290492f2c376a396e9f6998f8b5a492f01f
Anmol Arora <anmolarora1711@gmail.com>
2020-08-06 13:35:09 +0530

    Code

--- 2PlayerTicTacToe.py
+++ 2PlayerTicTacToe.py
@@ -3,13 +3,14 @@ import random
 print("TIC_TAC_TOE".center(120))
 player_1Choice = input("Choose your symbol- [X / 0]: ")
 player_2Choice = "X" if player_1Choice == "0" else "0"
- 
+
 print(f"Player 1 : {player_1Choice}   Player 2 : {player_2Choice}")
 
-gameProgress = [1,2,3,4,5,6,7,8,9]
-availablePositions = [1,2,3,4,5,6,7,8,9]
+gameProgress = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+availablePositions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # winningPositions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-winningPositions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+winningPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [
+    0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 player_1Wins = 0
 player_2Wins = 0
 gameDraw = 0
@@ -19,7 +20,7 @@ gameOver = False
 msg = "GAME DRAW"
 
 
-gameBoard=f'''
+gameBoard = f'''
        {gameProgress[0]}  |  {gameProgress[1]}  |  {gameProgress[2]}
        -------------
        {gameProgress[3]}  |  {gameProgress[4]}  |  {gameProgress[5]}
@@ -27,7 +28,7 @@ gameBoard=f'''
        {gameProgress[6]}  |  {gameProgress[7]}  |  {gameProgress[8]}
        '''
 print(gameBoard)
-while not gameOver and len(availablePositions) > 0 :
+while not gameOver and len(availablePositions) > 0:
     if player_1Turn:
         player_1Input = int(input("Player 1 \nEnter the position : "))
         if player_1Input not in availablePositions:
@@ -37,22 +38,22 @@ while not gameOver and len(availablePositions) > 0 :
         gameProgress[player_1Input - 1] = player_1Choice
         turnsPlayed += 1
         # player_1Turn = False
-        # print(gameBoard) #list index out of range 
-        # sirf ek if me rkhne se turnplayed vali condition nhi chl rhi 
-        # ye gameProgress[position[0]] vagarh vali line nhi aayi smjh me 
-        if turnsPlayed >= 3:
-            # print("Entry 1")
-            for position in winningPositions:
-                # print("Entry 2")
-                if gameProgress[position[0]] == gameProgress[position[1]] and gameProgress[position[1]] == gameProgress[position[2]]:
-                    # print("Entry 3")
-                    gameOver = True
-                    if player_1Turn:
-                        msg="Player 1 Won"
-                    else:
-                        msg="Player 2 Won" 
-                    break
-        
+        # print(gameBoard) #list index out of range
+        # sirf ek if me rkhne se turnplayed vali condition nhi chl rhi
+        # ye gameProgress[position[0]] vagarh vali line nhi aayi smjh me
+        # if turnsPlayed >= 3:
+        #     # print("Entry 1")
+        #     for position in winningPositions:
+        #         # print("Entry 2")
+        #         if gameProgress[position[0]] == gameProgress[position[1]] and gameProgress[position[1]] == gameProgress[position[2]]:
+        #             # print("Entry 3")
+        #             gameOver = True
+        #             if player_1Turn:
+        #                 msg="Player 1 Won"
+        #             else:
+        #                 msg="Player 2 Won"
+        #             break
+
     else:
 
         player_2Input = int(input("Player 2 \nEnter the position : "))
@@ -62,24 +63,8 @@ while not gameOver and len(availablePositions) > 0 :
         availablePositions.remove(player_2Input)
         gameProgress[player_2Input - 1] = player_2Choice
         # player_1Turn = True
-        if turnsPlayed >= 3:
-            # print("Entry 1")
-            for position in winningPositions:
-                # print("Entry 2")
-                if gameProgress[position[0]] == gameProgress[position[1]] and gameProgress[position[1]] == gameProgress[position[2]]:
-                    # print("Entry 3")
-                    gameOver = True
-                    if player_1Turn:
-                        msg="Player 1 Won"
-                    else:
-                        msg="Player 2 Won" 
-                    break
 
-        
-    
-    
-    player_1Turn = not player_1Turn
-    gameBoard=f'''
+    gameBoard = f'''
     {gameProgress[0]}  |  {gameProgress[1]}  |  {gameProgress[2]}
     ------------
     {gameProgress[3]}  |  {gameProgress[4]}  |  {gameProgress[5]}
@@ -87,9 +72,20 @@ while not gameOver and len(availablePositions) > 0 :
     {gameProgress[6]}  |  {gameProgress[7]}  |  {gameProgress[8]}
     '''
 
+    if turnsPlayed >= 3:
+        # print("Entry 1")
+        for position in winningPositions:
+            # print("Entry 2")
+            if gameProgress[position[0]] == gameProgress[position[1]] and gameProgress[position[1]] == gameProgress[position[2]]:
+                # print("Entry 3")
+                gameOver = True
+                if player_1Turn:
+                    msg = "Player 1 Won"
+                else:
+                    msg = "Player 2 Won"
+                break
 
     print(gameBoard)
+    player_1Turn = not player_1Turn
 
 print(msg)
-    
-
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -39,3 +39,40 @@ isGameFinished = False
 while not isGameFinished and userTurnsPlayed < 5:
     if isUserTurn:
         userInput = int(input("Enter the position: "))
+        if userInput in gamePositions:
+            gamePositions[userInput - 1] = userChoice
+            userTurnsPlayed += 1
+            isUserTurn = False
+        else:
+            print("Position already occupied...")
+            continue
+    else:
+        cpuInput = random.choice(gamePositions)
+        print("cpu input is", cpuInput)
+        if cpuInput == "X" or cpuInput == "O":
+            continue
+        gamePositions[cpuInput - 1] = cpuChoice
+        isUserTurn = True
+
+    print(f'''
+    {gamePositions[0]} | {gamePositions[1]} | {gamePositions[2]}
+    ---------
+    {gamePositions[3]} | {gamePositions[4]} | {gamePositions[5]}
+    ---------
+    {gamePositions[6]} | {gamePositions[7]} | {gamePositions[8]}
+    ''')
+
+    if userTurnsPlayed >= 3:
+        for position in winningPositions:
+            # print(position)   [0,1,2]   [3,4,5]
+            if gamePositions[position[0]] == gamePositions[position[1]] and gamePositions[position[0]] == gamePositions[position[2]]:
+                isGameFinished = True
+                print("CPU wins") if isUserTurn else print("User wins")
+                # if isUserTurn:
+                #     print("CPU wins")
+                # else:
+                #     print("User wins")
+                break
+
+if not isGameFinished:
+    print("Game draw")
